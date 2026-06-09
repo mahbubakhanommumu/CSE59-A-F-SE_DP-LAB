08671e49767da22d668ab445f2ff3ca04ca4289f
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-06-09 10:19:26 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,17 @@
+class Printer {
+    void printData(String text) {
+        System.out.println(text);
+    }
+    void printData(int number) {
+        System.out.println(number);       
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        
+        p.printData("Hello");
+        p.printData(100);
+    }
+}
