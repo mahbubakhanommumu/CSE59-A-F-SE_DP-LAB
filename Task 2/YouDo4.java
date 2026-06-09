09d58f55ae84654dd28446eb736984c6e34b4768
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-06-09 10:14:40 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,18 @@
+class Sensor {
+    String name = "Temperature Sensor";
+    int batteryLevel = 50;
+    
+    void charge() {
+        batteryLevel =100;
+        System.out.println("Sensor fully charged");
+    }
+}
 
+public class Main {
+    public static void main(String[] args){
+        Sensor s = new Sensor();
+        
+        s.charge();
+        System.out.println("Battery Level: " + s.batteryLevel);
+    }
+}
