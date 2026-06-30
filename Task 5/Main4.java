3d0a4609711fd54ce46feac426aabf0109e236c9
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-06-30 10:33:24 +0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,32 @@
 
+interface OptimizableModel {
+    void quantize();
+}
+
+class ResNet18 implements OptimizableModel {
+    public void quantize() {
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+class MobileNet implements OptimizableModel {
+    public void quantize(){
+        System.out.println("Applying dynamic quantization for MobileNet.");
+        
+    }
+}
+class InferenceEngine {
+    public void processModel(OptimizableModel model){
+        model.quantize();
+    }
+}
+public class Main{
+    public static void main(String [] args){
+        InferenceEngine engine = new InferenceEngine();
+        
+        OptimizableModel resnet = new ResNet18();
+        OptimizableModel mobile = new MobileNet();
+        
+        engine.processModel(resnet);
+        engine.processModel(mobile);
+    }
+}
