cb67a8148474704e50a96ee54f9c6990f280fed1
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-06-30 11:19:02 +0600

    Update Main6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,22 @@
 
+interface HttpService {
+    void handleHttpRequest();
+}
+interface DatabaseManager{
+    void executeSqlStatement();
+}
+interface ContainerOps{
+    void restartDockerContainer();
+}
+class WebController implements HttpService{
+    public void handleHttpRequest(){
+        System.out.println("Routing traffic to endpoint.");
+
+    }
+}
+public class Main6{
+    public static void main(String[] args) {
+        HttpService web= new WebController();
+        web.handleHttpRequest();
+    }
+}
