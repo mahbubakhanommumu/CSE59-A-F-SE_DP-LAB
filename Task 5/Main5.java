f4ec6284e39a0cf55a8f309b3cb5e6288e86ee79
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-06-30 10:59:26 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,25 @@
+class UniversityMember{
+    void attendLecture(){
+        System.out.println("Attending lecture.");
+    }
+}
+interface Researcher{
+    void conductLabResearch();
+}
+class Professor extends UniversityMember implements Researcher{
+    public void conductLabResearch(){
+        System.out.println("Professor doing research.");
+    }
+}
+class UndergraduateStudent extends UniversityMember{
 
+}
+public class Main5{
+    public static void main(String[] args){
+        UniversityMember u = new UndergraduateStudent();
+        u.attendLecture();
+
+        Researcher r= new Professor();
+        r.conductLabResearch();
+    }
+}
