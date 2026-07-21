bdb1a7cf9f6fe72c5a14586008abe23803e10eef
mahbubakhanommumu <mahbubakhanommumu@gmail.com>
2026-07-21 11:28:06 +0600

    Update Main4.java

--- Task 8/Main4.java
+++ Task 8/Main4.java
@@ -1,1 +1,21 @@
 
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+class ConfigurationManager {
+   private static volatile ConfigurationManager instance;
+
+   private ConfigurationManager() {
+      System.out.println("Reading heavy config file from disk...(Only happens once)");
+   }
+
+   public static ConfigurationManager getInstance() {
+      if (instance == null) {
+         synchronized(ConfigurationManager.class) {
+            if (instance == null) {
+               instance = new ConfigurationManager();
+            }
+         }
+      }
+
+      return instance;
+   }
+}
